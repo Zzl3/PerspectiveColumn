a528d2557f34ab2587fdf45860e1513e8801f2b6
Zzl3 <91377542+Zzl3@users.noreply.github.com>
2023-01-13 08:49:46 +0800

    代码

---
+++ parking1/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+body {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ parking1/src/views/ThreeJSView.vue
@@ -0,0 +1,127 @@
+<template>
+  <div id="console">
+    {{ camera_data }}
+  </div>
+  <br>
+  <div id="three"></div>
+</template>
+
+<script lang='ts'>
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+//创建一个三维场景
+const scene = new THREE.Scene()
+
+export default{
+  data () {
+    return {
+      camera: null,
+      renderer: null,
+      camera_data: null,
+    }
+  },
+  mounted () {
+    this.$nextTick(this.init);
+  },
+  methods: {
+    init(){
+      //添加光源
+      const ambient = new THREE.AmbientLight(0xffffff, 0.5)
+      const light1 = new THREE.PointLight(0xffffff, 0.4)
+      const light2 = new THREE.PointLight(0xffffff, 0.4)
+
+      scene.add(ambient)
+      light1.position.set(200,300,400)
+      scene.add(light1)
+      light2.position.set(-200,-300,-400)
+      scene.add(light2)
+
+      //创建一个透视相机
+      const width = window.innerWidth, height = window.innerHeight
+      this.camera = new THREE.PerspectiveCamera(45, width/height, 1, 1000)
+      //设置相机位置
+      this.camera.position.set(200,200,200) 
+      //设置相机方向
+      this.camera.lookAt(0,0,0)
+
+      // //创建辅助坐标轴
+      // const axesHelper = new THREE.AxesHelper(100)
+      // scene.add(axesHelper)
+
+      // //创建一个物体（形状）
+      // const geometry = new THREE.BoxGeometry(100, 100, 100)
+      // //创建材质（外观）
+      // const material = new THREE.MeshLambertMaterial({
+      //   color: 0x00ffff,//设置材质颜色
+      //   transparent: true,//开启透明度
+      //   opacity: 0.5 //设置透明度
+      // })
+      // //创建一个网格模型对象
+      // const mesh = new THREE.Mesh(geometry, material)
+      // //把网格模型添加到三维场景
+      // scene.add(mesh)
+
+      //创建一个WebGL渲染器
+      this.renderer = new THREE.WebGLRenderer()
+      this.renderer.setSize(width,height)
+      this.renderer.render(scene,this.camera)
+      document.getElementById('three')?.appendChild(this.renderer.domElement)
+
+      // 加载停车场地图模型
+      const loader = new GLTFLoader();
+      loader.load('model/park.glb', (gltf)=>{
+          //将模型添加至场景
+          scene.add(gltf.scene)
+          //设置模型位置
+          gltf.scene.position.set(0,0,0)
+          this.renderer.render(scene, this.camera)
+          this.camera_data = this.camera.position
+      })
+      loader.load('model/objects/Audi_A8_2014_2.glb', (gltf)=>{
+          //将模型添加至场景
+          scene.add(gltf.scene)
+          //设置模型位置
+          gltf.scene.position.set(0,0,0)
+          //设置模型运动
+          setInterval(()=>{
+            gltf.scene.position.x += 0.02
+            console.log(gltf.scene.position.x)
+            if (gltf.scene.position.x>5){ gltf.scene.position.x = 0 }
+            this.renderer.render(scene, this.camera)
+          },20)
+
+          this.renderer.render(scene, this.camera)
+      })
+
+      //鼠标控制
+      const controls = new OrbitControls(this.camera, this.renderer.domElement)
+      //添加阻尼
+      controls.enableDamping = true
+	    controls.dampingFactor = 0.1
+      //监听鼠标事件
+      controls.addEventListener('change',()=>{
+        this.renderer.render(scene, this.camera)
+        this.camera_data = this.camera.position
+      })
+    }
+  }
+}
+
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#three {
+  width: 100%;
+  height: 90%;
+  /* display: flex; */
+}
+#console {
+  width: 100%;
+  height: 10%;
+  /* display: flex; */
+}
+</style>
